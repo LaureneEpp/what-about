5445b92fcd547768fb5d48177c71faddd1cc2f95
LaureneEpp <laurene.eppelle@gmail.com>
2023-02-20 14:31:20 +0100

    new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_17_151331) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_20_120442) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_17_151331) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "subtitle"
+    t.string "state", default: "draft"
+    t.string "publisher"
+    t.datetime "published_at"
   end
 
 end
