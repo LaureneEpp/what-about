c372ab872f28379891cb670dd07c7cb5b60dbcad
LaureneEpp <laurene.eppelle@gmail.com>
2023-02-22 13:35:33 +0100

    Add FriendlyID + Create User controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  get 'static_pages/landing_page'
-  get 'static_pages/dashboard'
-  devise_for :users
-  resources :posts 
+  get 'users/index'
+  get 'users/show'
   # root 'posts#index'
   root 'static_pages#landing_page'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+resources :users, :only =>[:show, :index]
+  # devise_for :users
+  get 'static_pages/landing_page'
+  get 'static_pages/dashboard'
+
+  resources :posts 
 end
