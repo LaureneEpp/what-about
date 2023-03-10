30074e24ce06c487d9f26e9d0374ffdfc95616db
LaureneEpp <laurene.eppelle@gmail.com>
2023-03-10 13:35:38 +0100

    refactoring

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,6 @@ class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:landing_page]
 
   def landing_page
-    @q = Post.ransack(params[:q])
-    @posts = @q.result(distinct: true)
   end
 
   def dashboard
