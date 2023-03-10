ff92223c3c14011da10cc1d095012fa4ca74bfcd
LaureneEpp <laurene.eppelle@gmail.com>
2023-03-10 22:22:13 +0100

    new-migration

--- db/migrate/20230310212135_add_info_to_users.rb
+++ db/migrate/20230310212135_add_info_to_users.rb
@@ -1,4 +1,4 @@
-class AddUsernameToUser < ActiveRecord::Migration[7.0]
+class AddInfoToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :username, :string
     rename_column :users, :mast_name, :last_name
