000fccb36b1878bb96d11f61c354d5edc471d40c
LaureneEpp <laurene.eppelle@gmail.com>
2023-03-30 00:46:27 +0200

    adding friendlyID to Rooms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_27_155702) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_29_223205) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -139,6 +139,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_155702) do
     t.boolean "is_private", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_rooms_on_slug", unique: true
   end
 
   create_table "users", force: :cascade do |t|
