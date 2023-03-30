9f4b78d7631e446ad45db33965188b89ca844321
LaureneEpp <laurene.eppelle@gmail.com>
2023-03-30 13:49:37 +0200

    create account page for current_user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,13 +1,8 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get users_index_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get users_show_url
+  test "should get account" do
+    get users_account_url
     assert_response :success
   end
 end
